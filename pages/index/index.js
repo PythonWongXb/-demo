cf8f3f6c5db5a851574ced3030bce1edaba8142a
Pythonista WangXB <739221432@qq.com>
2020-05-05 16:07:23 +0800

    '增加微信登录功能'

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,6 +13,98 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+
+  getUserInfoWechat: function(e) {
+    
+    appInstance.globalData.userInfo = e.detail.userInfo
+    // console.log(appInstance.globalData.userInfo)
+  },
+  
+  login:function(){
+    
+    var that = this;
+    that.setData({wechatlogin: true})
+    wx.login({
+      success (res) {
+        // console.log(res)
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'http://192.168.1.104:8888/api/v1/payment/openid/',
+            data: {
+              code: res.code
+            },
+            method:"GET",
+            success(res){
+              // console.log(res)
+              appInstance.globalData.openid = res.data.openid
+              appInstance.globalData.session_key = res.data.session_key
+
+              console.log(appInstance.globalData)
+              // console.log(appInstance.globalData.session_key)
+              // console.log(appInstance.globalData.userInfo.nickName)
+              wx.request({
+                url: 'http://192.168.1.104:8888/api/v1/users/CreatUserByWeChat',
+                method: "POST",
+                data:{
+                  full_name: appInstance.globalData.userInfo.nickName,
+                  key: appInstance.globalData.openid+"wxb",
+                  openid: appInstance.globalData.openid+"wxb"
+                },
+                success(res){
+                  
+                  if (res.statusCode==200){
+                    console.log('创建成功')
+                    
+                  }else{
+                    console.log('用户已存在')
+                  }
+    
+                    wx.request({
+                     url: 'http://192.168.1.104:8888/api/v1/login/access-token',
+                     data: {
+                      "username": appInstance.globalData.openid+"wxb",
+                      "password": appInstance.globalData.openid+"wxb",
+                      "scope":that.data.imagecode + ' ' +that.data.answer
+                       },
+                      method: 'POST',
+                      header: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                      },
+                      success: function (res) {
+  
+                      that.setData({result:JSON.stringify(res.data.detail)})
+                
+                  
+                      if (res.statusCode === 200){
+                      that.setData({result:'登录成功'})
+                      appInstance.globalData.cookie = "Bearer "+res.data.access_token
+                      console.log(appInstance.globalData)
+                       wx.navigateTo({
+                         url: '../center/center',
+                       })
+                      }
+                    },
+                     
+                    })
+                   
+                 
+                }
+              })
+            }
+          })
+          
+        } else {
+          console.log('登录失败！')
+        }
+        
+      }
+    })
+      
+   
+  },
+
+
   contactus:function(){
     wx.navigateTo({
       url: '../admin/contactus',
@@ -54,7 +146,6 @@ Page({
       }
     },
      
-      
     })
    },
    guid: function(){
